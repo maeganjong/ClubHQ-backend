67f43b96ccd876f834c3be166635b179bf3f9792
Amy Zhou <amyzhou@college.harvard.edu>
2020-01-21 16:49:51 -0500

    amyzhou3

---
+++ data/clubs.js
@@ -0,0 +1,20 @@
+const casual = require('casual')
+
+casual.define('club', () => ({
+  id: casual.uuid,
+  name: casual.title,
+  email: casual.email,
+  website: casual.url,
+  size: casual.random_element(['SMALL', 'MEDIUM', 'LARGE']),
+  summary: casual.sentences,
+  competitiveness: casual.random_element(['COMPETITIVE', 'COMPLETION']),
+}))
+
+
+const clubsData = []
+
+for (let i = 0; i < 20; ++i) {
+  clubsData.push(casual.club())
+}
+
+module.exports = clubsData
---
+++ data/tagClubs.js
@@ -0,0 +1,19 @@
+const casual = require('casual')
+const clubsData = require('./clubs')
+const tagsData = require('./tags')
+
+casual.define('tagClub', (clubId, tagId) => ({
+  clubId,
+  tagId,
+
+}))
+
+const tagClubData = []
+
+for (let i = 0; i < 20; ++i) {
+  const clubId = casual.random_element(clubsData).id
+  const tagId = casual.random_element(tagsData).id
+  tagClubData.push(casual.friend(clubId, tagId))
+}
+
+module.exports = tagClubData
