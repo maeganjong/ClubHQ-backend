db65ff0341bc4b3c187de65de495e93d377c967c
Maegan Jong <maegan.jong@gmail.com>
2020-01-23 16:31:52 -0500

    new clubs

--- src/db/migrations/20200121105725_clubs.js
+++ src/db/migrations/20200121105725_clubs.js
@@ -19,7 +19,7 @@ exports.up = knex => knex.schema.createTable('clubs', table => {
   table.string('summary')
 
   table
-    .enum('competitiveness', ['COMPETITIVE', 'COMPLETION'])
+    .enum('competitiveness', ['COMPETITIVE', 'COMPLETION', 'N/A'])
 
   table.timestamp('createdAt').defaultTo(knex.fn.now())
   table.timestamp('updatedAt').defaultTo(knex.fn.now())
